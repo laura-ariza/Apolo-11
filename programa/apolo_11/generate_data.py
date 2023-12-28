f1ee2875cd601a0e0f5c25b96f8bbce9db75fdca
santiagoml2 <santiagoml2@hotmail.com>
2023-12-27 19:08:30 -0500

    yaml file generation and simulator folder ready to commit

--- programa/apolo_11/generate_data.py
+++ programa/apolo_11/generate_data.py
@@ -7,6 +7,7 @@ import random
 #https://docs.python.org/es/3/library/hashlib.html
 #Hashlib:this module implements a common interface to many different secure hash and message digest algorithms.
 import hashlib
+import yaml
 
 def generate_data():
 
@@ -25,16 +26,30 @@ def generate_data():
         #concatenate relevant data to generate the hash
         hash_data = f"{now_date}{value_mission}{device}{device_status}"
         #apply hash SHA-256 to concatenated data
-        hash = hashlib.sha256(hash_data.encode()).hexdigest()
+        hash_value = hashlib.sha256(hash_data.encode()).hexdigest()
 
-    files = f"APL{mission}-0000{random.randint(1, 1000)}.log"
+    #files = f"APL{mission}-0000{random.randint(1, 1000)}.log"
+    files = f"APL{mission}-0000{random.randint(1, 1000)}.yaml"
     
+    '''
     with open(files, 'w') as file:
         file.write(f"Date: {now_date}\n")
         file.write(f"Mission: {value_mission}\n")
         file.write(f"Device: {device}\n")
         file.write(f"Device status: {device_status}\n")
         file.write(f"Hash: {hash}\n")
+    '''
+    
+    with open(files, 'w') as file:
+        data = {
+            'Date': now_date,
+            'Mission': value_mission,
+            'Device': device,
+            'Device Status': device_status,
+            'Hash': hash_value
+        }
+        yaml.dump(data, file)        
+        
 
 min: int = 1 
 max: int = 3
