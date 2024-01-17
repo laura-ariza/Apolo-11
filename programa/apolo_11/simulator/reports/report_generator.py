4141c0970fb9cd96f8a2311ca54b28241909f7d6
santiagoml2 <santiagoml2@hotmail.com>
2024-01-17 11:07:42 -0500

    retrieving dictionaries from all json files

--- programa/apolo_11/simulator/reports/report_generator.py
+++ programa/apolo_11/simulator/reports/report_generator.py
@@ -1,18 +1,12 @@
 
 import os
 import json
-print("Current working directory:", os.getcwd())
 
-def extract_device_info(file_path):
+def extract_all_keys(file_path):
     with open(file_path, 'r') as file:
         try:
             data = json.load(file)
-            device_info = data.get('Mission')
-            if device_info:
-                return device_info
-            else:
-                print(f"No 'device' key found in {file_path}")
-                return None
+            return data
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON in {file_path}: {e}")
             return None
@@ -23,9 +17,9 @@ def process_files(directory):
         for filename in files:
             if filename.endswith(".log") or filename.endswith(".json"):
                 file_path = os.path.join(root, filename)
-                device_info = extract_device_info(file_path)
-                if device_info is not None:
-                    extracted_info.append(device_info)
+                file_data = extract_all_keys(file_path)
+                if file_data is not None:
+                    extracted_info.append(file_data)
     return extracted_info
 
 # Replace 'your_directory_path' with the path to the directory containing your JSON files
@@ -34,4 +28,4 @@ result = process_files(directory_path)
 
 # Print the extracted information
 for idx, info in enumerate(result, start=1):
-    print(f"File {idx}: {info}")
+    print(f"File {idx}:\n{info}")
