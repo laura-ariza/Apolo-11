47afe2cc7fba636f650aaec6b2cd38d23bb83147
tatianaochoab <tata8abautista@gmail.com>
2024-01-21 22:58:23 -0500

    adjust folder tree, run and add .gitignore

--- programa/apolo_11/simulator/launcher.py
+++ programa/apolo_11/simulator/launcher.py
@@ -3,19 +3,31 @@ import generate as gd
 import os
 from datetime import datetime
 
+
+count_executed: int = 1
+
+
 #create a directory and return the directory path
 def new_dir():
-    dir_name = "dir_" + str(datetime.now()).replace(":", "_") #definir reglas de nombre de carpetas
+    dir_name = "simulation_" + str(datetime.now()).replace(":", "_") #definir reglas de nombre de carpetas
     dir_devices = "devices"
     absolute_path = os.path.dirname(__file__)
+    print("esto es lo que imprime: ", absolute_path)
     dir_path = ""
-    if not os.path.isdir(dir_devices):
-        os.mkdir(dir_devices)
     dir_path = os.path.join(absolute_path, dir_devices)
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
     dir_path = os.path.join(dir_path, dir_name)
     os.mkdir(dir_path)
     print("este es el path" + dir_path)
     return dir_path
+
+def dir_executed(dir_path):
+    global count_executed
+    new_dir_path = os.path.join(dir_path, f"executed_{count_executed}")
+    count_executed += 1
+    os.mkdir(new_dir_path)
+    return new_dir_path
 
 def run_file(file_path):
     try:
@@ -30,8 +42,9 @@ def main():
     print("Press 'Ctrl+C' to stop the program.")
     
     #file_path = "../apolo_11/path_test.py"
-    dir_path = new_dir()
+    dir_path_simulator = new_dir()
     while True:
+        dir_path = dir_executed(dir_path_simulator)
         gd.files_create(dir_path)
         #run_file(file_path)
         time.sleep(5)
